48e2d6984e4f1bc64cb0fff8b010b0a7c7a1f6b7
Kevin Lucich <lucichkevin@gmail.com>
2014-09-11 14:47:29 +0200

    Revert "Revert "[#12] app/src/main/java/com/ilsecondodasinistra/workitout/utils/CounterWorkingTime.java""
    This reverts commit 4741c58a98ffb5ca9f70100f614a74193c6e48df.

--- app/src/main/java/com/ilsecondodasinistra/workitout/MyWidgetProvider.java
+++ app/src/main/java/com/ilsecondodasinistra/workitout/MyWidgetProvider.java
@@ -6,54 +6,67 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.RemoteViews;
 
-import java.util.Random;
+import com.ilsecondodasinistra.workitout.utils.BadgeHelper;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import it.lucichkevin.cip.Utils;
 
 public class MyWidgetProvider extends AppWidgetProvider {
 
-	private static final String ACTION_CLICK = "ACTION_CLICK";
+    private static final String ACTION_BADGE = "action_badge";
 
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-			int[] appWidgetIds) {
-		
-	    // Get all ids
-	    ComponentName thisWidget = new ComponentName(context,
-	        MyWidgetProvider.class);
+	public void onUpdate( Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds ){
+
+	    //  Get all ids
+	    ComponentName thisWidget = new ComponentName( context, MyWidgetProvider.class );
 	    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-	    for (int widgetId : allWidgetIds) {
-	      // Create some random data
-	      int number = (new Random().nextInt(100));
 
-	      RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-	          R.layout.widget_layout);
-	      Log.w("WidgetExample", String.valueOf(number));
-	      // Set the text
-	      remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+        for( int widgetId : allWidgetIds ){
 
-	      // Register an onClickListener
-	      Intent intent = new Intent(context, MyWidgetProvider.class);
-//	      Intent intent = new Intent(context, WorkItOutMain.class);
+	        RemoteViews remoteViews = new RemoteViews( context.getPackageName(), R.layout.widget_layout );
 
-	      intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-	      intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+	        //    Register an onClickListener
+	        Intent intent = new Intent( context, MyWidgetProvider.class );
+            intent.setAction(MyWidgetProvider.ACTION_BADGE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
 
-	      PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-	          0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-	      remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
-	      appWidgetManager.updateAppWidget(widgetId, remoteViews);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast( context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+	        remoteViews.setOnClickPendingIntent( R.id.btn_badge, pendingIntent );
+	        appWidgetManager.updateAppWidget( widgetId, remoteViews );
 	    }
 	}
-	
+
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
+	public void onReceive( Context context, Intent intent ){
 		super.onReceive(context, intent);
-		
-		if (intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
-			   //do some really cool stuff here
-			}
+
+        Utils.logger("onReceive() :D  intent.getAction() = "+ intent.getAction(), Utils.LOG_DEBUG );
+
+		if( intent.getAction().equals(MyWidgetProvider.ACTION_BADGE) ){
+            Utils.logger("ho premuto il widget! :D", Utils.LOG_DEBUG );
+
+            Calendar now = Calendar.getInstance();
+
+            Date date = BadgeHelper.getEntranceTime();
+            Utils.logger( "EntranceTime = "+ date.getHours() +":"+ date.getMinutes(), Utils.LOG_DEBUG );
+
+            date = now.getTime();
+            Utils.logger( "setEntrance = "+ date.getHours() +":"+ date.getMinutes(), Utils.LOG_DEBUG );
+
+//            Utils.Toaster( context, "EntranceTime = "+ date.getHours() +":"+ date.getMinutes() );
+        }
+
 	}
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        //  Quando viene eliminato...
+        Utils.Toaster(context, "Perchè mi abbandoni?!", Utils.Toaster.LENGTH_SHORT);
+    }
 }
