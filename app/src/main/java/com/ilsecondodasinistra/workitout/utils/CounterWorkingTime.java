ee730b51f13503d327727bae66e959a42106e94b
MarKco <ilsecondodasinistra@gmail.com>
2015-03-21 23:29:27 +0100

    Revert "Revert "Merge branch 'kevinoo-master'""
    This reverts commit b7b6328e29754e72a2e1231c7a16b3f67fe194e6.

--- app/src/main/java/com/ilsecondodasinistra/workitout/utils/CounterWorkingTime.java
+++ app/src/main/java/com/ilsecondodasinistra/workitout/utils/CounterWorkingTime.java
@@ -9,13 +9,10 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import it.lucichkevin.cip.Utils;
 
 
 /**
- *  @author     Kevin Lucich (11/09/14)
+ * @author Kevin Lucich (11/09/14)
  */
 public class CounterWorkingTime {
 
@@ -28,34 +25,34 @@ public class CounterWorkingTime {
     private TextView textView;
     private Long currentMillis;
 
-    public CounterWorkingTime( TextView textView ){
+    public CounterWorkingTime(TextView textView) {
         this.textView = textView;
     }
 
-    public void calculateMillis(){
+    public void calculateMillis() {
 //        stop();
         Long time = BadgeHelper.getCurrentSessionWorking().calcExitTime();
-        if( time == null ){
+        if (time == null) {
             currentMillis = null;
             return;
         }
         currentMillis = DateTime.now(DateTimeZone.UTC).getMillis() - time;
     }
 
-    public void restart(){
+    public void restart() {
         stop();
         start();
     }
 
     public synchronized final CounterWorkingTime start() {
 
-        if( STARTED ){
+        if (STARTED) {
             return this;
         }
 
         calculateMillis();
 
-        if( currentMillis == null ){
+        if (currentMillis == null) {
             return this;
         }
 
@@ -65,8 +62,8 @@ public class CounterWorkingTime {
         return this;
     }
 
-    public CounterWorkingTime stop(){
-        if( !STARTED ){
+    public CounterWorkingTime stop() {
+        if (!STARTED) {
             return this;
         }
         STARTED = false;
@@ -74,30 +71,30 @@ public class CounterWorkingTime {
         return this;
     }
 
-    public void onTick( long millis ){
-        textView.setText( format(millis) );
+    public void onTick(long millis) {
+        textView.setText(format(millis));
     }
 
-    private String format( long millis ){
+    private String format(long millis) {
 
         int seconds = (int) millis / 1000;
 
-        if( seconds < 0 ){
+        if (seconds < 0) {
             seconds *= -1;
         }
 
         int hours = seconds / 3600;
-        int minutes = (seconds - hours*3600) / 60;
-        seconds = seconds - (hours*3600) - (minutes*60);
+        int minutes = (seconds - hours * 3600) / 60;
+        seconds = seconds - (hours * 3600) - (minutes * 60);
 
-        return ((millis < 0)?"-":"") + pad(hours) +":"+ pad(minutes) +":"+ pad(seconds);
+        return ((millis < 0) ? "-" : "") + pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
     }
 
-    private String pad( int n ){
-        if( n < 10 ){
-            return "0"+ n;
+    private String pad(int n) {
+        if (n < 10) {
+            return "0" + n;
         }
-        return ""+ n;
+        return "" + n;
     }
 
     private Handler mHandler = new Handler() {
@@ -116,7 +113,7 @@ public class CounterWorkingTime {
 
             // special case: user's onTick took more than interval to
             // complete, skip to next interval
-            while( delay < 0 ){
+            while (delay < 0) {
                 delay += interval;
             }
 
@@ -129,7 +126,7 @@ public class CounterWorkingTime {
     ///////////////////////////
     //  Getters and Setter
 
-    public TextView getTextView(){
+    public TextView getTextView() {
         return textView;
     }
 
--- app/src/main/java/com/ilsecondodasinistra/workitout/utils/SettingsWorkitout.java
+++ app/src/main/java/com/ilsecondodasinistra/workitout/utils/SettingsWorkitout.java
@@ -2,11 +2,10 @@ package com.ilsecondodasinistra.workitout.utils;
 
 import org.joda.time.Duration;
 
-import it.lucichkevin.cip.Utils;
 import it.lucichkevin.cip.preferencesmanager.PreferencesManager;
 
 /**
- *  @author     Kevin Lucich (29/08/14)
+ * @author Kevin Lucich (29/08/14)
  */
 public class SettingsWorkitout extends PreferencesManager {
 
@@ -21,50 +20,56 @@ public class SettingsWorkitout extends PreferencesManager {
     public static final String PAUSE_DURATION = "pause_duration";  //  Minutes
     public static final String MINIMUM_PAUSE_DURATION = "minimum_pause_duration";  //  Minutes
     public static final String NOTIFICATIONS_ENABLED = "notifications_enabled";
-    public static final String PAUSE_COUNTED_FOR_EXIT = "notifications_enabled";
+    public static final String PAUSE_COUNTED_FOR_EXIT = "pause_counted_for_exit";
 
-    public static int getPauseDuration( int default_name ){
-        return getPreferences().getInt(PAUSE_DURATION, default_name );
+    public static int getPauseDuration(int default_name) {
+        return getPreferences().getInt(PAUSE_DURATION, default_name);
     }
-    public static int getPauseDuration(){
+
+    public static int getPauseDuration() {
         return getPauseDuration(15);
     }
 
-    public static void setWorkTime( Duration work_time_duration ){
-        //  Save in milliseconds
-        setPreferences(WORK_TIME, work_time_duration.getMillis() );
+    public static void setWorkTime(Duration work_time_duration) {
+        //  Save in minutes
+        setPreferences(WORK_TIME, ((int) work_time_duration.getMillis() / 60000));
     }
-    public static Duration getWorkTime( int default_value ){
-        int minutes = getPreferences().getInt(WORK_TIME, default_value );
-        return new Duration(minutes*60000);
+
+    public static Duration getWorkTime(int default_value) {
+        int minutes = getPreferences().getInt(WORK_TIME, default_value);
+        return new Duration(minutes * 60000);
     }
-    public static Duration getWorkTime(){
+
+    public static Duration getWorkTime() {
         //  Default 8 ore :)
         return getWorkTime(480);
     }
 
     /**
-     *  Return the minimum duration of lunch pause
+     * Return the minimum duration of lunch pause
      */
-    public static Duration getMinimumLunchPause( int default_minute ){
-        int minutes = getPreferences().getInt( MINIMUM_PAUSE_DURATION, default_minute );
-        return new Duration(minutes*60000);
+    public static Duration getMinimumLunchPause(int default_minute) {
+        int minutes = getPreferences().getInt(MINIMUM_PAUSE_DURATION, default_minute);
+        return new Duration(minutes * 60000);
     }
-    public static Duration getMinimumLunchPause(){
+
+    public static Duration getMinimumLunchPause() {
         return getMinimumLunchPause(30);
     }
 
-    public static boolean isNotificationsEnabled( boolean default_value ){
-        return getPreferences().getBoolean( NOTIFICATIONS_ENABLED, default_value );
+    public static boolean isNotificationsEnabled(boolean default_value) {
+        return getPreferences().getBoolean(NOTIFICATIONS_ENABLED, default_value);
     }
-    public static boolean isNotificationsEnabled(){
+
+    public static boolean isNotificationsEnabled() {
         return isNotificationsEnabled(true);
     }
 
-    public static boolean isPausesCounted( boolean default_value ){
-        return getPreferences().getBoolean( PAUSE_COUNTED_FOR_EXIT, default_value );
+    public static boolean isPauseCounted(boolean default_value) {
+        return getPreferences().getBoolean(PAUSE_COUNTED_FOR_EXIT, default_value);
     }
-    public static boolean isPausesCounted(){
+
+    public static boolean isPauseCounted() {
         return isNotificationsEnabled(false);
     }
 
